3f4179f09840549d87852ff6bda74b748af1dd91
Martin Chobanyan <chobanyan.martin@gmail.com>
2019-07-18 09:37:39 -0400

    added fluorine neighborhood

--- pairwise_features2.py
+++ pairwise_features2.py
@@ -14,33 +14,33 @@ from chem_math import find_atomic_path, vectorized_dihedral_angle
 #                               Define the helper functions to get/calculate the features
 ########################################################################################################################
 
+
 def get_distance(molecule_map, molecule_name, a0, a1):
+    if a0 == -1 or a1 == -1:
+        return -1
     distance_matrix = molecule_map[molecule_name]['distance_matrix']
     return distance_matrix[a0, a1]
 
 
 def calculate_neighborhood(molecule_map, molecule_name, atom_idx):
-    H, C, N, O = 4 * [0]
+    if atom_idx == -1:
+        return [-1, -1, -1, -1]
+
+    neighbor_count = {'H': 0, 'C': 0, 'N': 0, 'O': 0, 'F': 0}
     molecule = molecule_map[molecule_name]['rdkit']
     atom = molecule.GetAtomWithIdx(atom_idx)
-    hydrogen_root = (atom.GetSymbol() == 'H')
 
     for neighbor in atom.GetNeighbors():
         symbol = neighbor.GetSymbol()
-        if symbol == 'H':
-            H += 1
-        elif symbol == 'C':
-            C += 1
-        elif symbol == 'N':
-            N += 1
-        elif symbol == 'O':
-            O += 1
+        neighbor_count[symbol] += 1
 
-    neighborhood = [C, N, O] if hydrogen_root else [H, C, N, O]
-    return neighborhood
+    return [neighbor_count['H'], neighbor_count['C'], neighbor_count['N'], neighbor_count['O'], neighbor_count['F']]
 
 
 def find_hybridization(molecule_map, molec_name, atom_idx):
+    if atom_idx == -1:
+        return [-1, -1, -1]
+
     molecule = molecule_map[molec_name]['rdkit']
     atom = molecule.GetAtomWithIdx(atom_idx)
     hybridization = str(atom.GetHybridization())
@@ -58,6 +58,9 @@ def find_hybridization(molecule_map, molec_name, atom_idx):
 
 def encode_inner_element(molecule_map, molec_name, atom_idx):
     """Determine and one-hot encode the element of an atom that is not on the peripheries of the molecule"""
+    if atom_idx == -1:
+        return [-1, -1]
+
     molecule = molecule_map[molec_name]['rdkit']
     symbol = molecule.GetAtomWithIdx(atom_idx).GetSymbol()
     if symbol == 'C':
@@ -110,16 +113,17 @@ def calculate_dihedral_angles(df, molecule_map, start_col, x_col, y_col, end_col
     df['dihedral'] = 0.0
     for i in range(0, end, step):
         print(f'Step {int(i / step)} / {int(end / step)}')
-        df.iloc[i:i + step - 1, -1] = vectorized_dihedral_angle(coords[start_col][i:i + step],
-                                                                coords[x_col][i:i + step],
-                                                                coords[y_col][i:i + step],
-                                                                coords[end_col][i:i + step])
+        df.loc[i:i + step - 1, 'dihedral'] = vectorized_dihedral_angle(coords[start_col][i:i + step],
+                                                                       coords[x_col][i:i + step],
+                                                                       coords[y_col][i:i + step],
+                                                                       coords[end_col][i:i + step])
     return df
 
 
 ########################################################################################################################
 #                  Define the objects to extract the appropriate features from each coupling type
 ########################################################################################################################
+
 
 class FeatureEngineer(object):
     """A base class for the feature engineering objects across coupling types
@@ -138,8 +142,9 @@ class FeatureEngineer(object):
         return df
 
     def get_neighbors(self, df, atom_idx_col, col_names):
-        neighbors = df.apply(lambda x: calculate_neighborhood(self.molecule_map, x['molecule_name'], x[atom_idx_col]),
-                             axis=1)
+        neighbors = df.apply(
+            lambda x: calculate_neighborhood(self.molecule_map, x['molecule_name'], x[atom_idx_col]),
+            axis=1)
         neighbors = pd.DataFrame(neighbors.values.tolist(), columns=col_names)
         df = pd.concat([df, neighbors], axis=1)
         return df
@@ -183,13 +188,14 @@ class Prepare1JH_(FeatureEngineer):
 
     @staticmethod
     def feature_cols():
-        return ['distance', 'H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors', 'sp', 'sp2', 'sp3']
+        return ['distance', 'H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors', 'F_neighbors',
+                'sp', 'sp2', 'sp3']
 
     def __call__(self, df):
         df = super().__call__(df)
 
         # get the neighbor distribution
-        neighbor_cols = ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors']
+        neighbor_cols = ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors', 'F_neighbors']
         df = self.get_neighbors(df, 'atom_index_1', neighbor_cols)
 
         # get the hybridization
@@ -213,7 +219,7 @@ class Prepare2JHH(FeatureEngineer):
     @staticmethod
     def feature_cols():
         return ['distance', 'distance_hx', 'x_nitrogen', 'x_oxygen',
-                'x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors',
+                'x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors', 'x_F_neighbors',
                 'x_sp', 'x_sp2', 'x_sp3']
 
     def __call__(self, df):
@@ -232,7 +238,7 @@ class Prepare2JHH(FeatureEngineer):
         df = self.get_element_encodings(df, 'atom_index_x', x_element_cols)
 
         # get the neighbor distribution of atom X
-        neighbor_cols = ['x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors']
+        neighbor_cols = ['x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors', 'x_F_neighbors']
         df = self.get_neighbors(df, 'atom_index_x', neighbor_cols)
 
         # get the hybridization of atom X
@@ -255,7 +261,7 @@ class Prepare2JH_(Prepare2JHH):
     @staticmethod
     def feature_cols():
         base_cols = Prepare2JHH.feature_cols()
-        return base_cols + ['distance_x_', 'H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors',
+        return base_cols + ['distance_x_', 'H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors', 'F_neighbors',
                             'sp', 'sp2', 'sp3']
 
     def __call__(self, df):
@@ -263,7 +269,7 @@ class Prepare2JH_(Prepare2JHH):
 
         df = self.get_distances(df, 'atom_index_x', 'atom_index_1', 'distance_x_')
 
-        neighbor_cols = ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors']
+        neighbor_cols = ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors', 'F_neighbors']
         df = self.get_neighbors(df, 'atom_index_1', neighbor_cols)
 
         hybrid_cols = ['sp', 'sp2', 'sp3']
@@ -297,8 +303,8 @@ class Prepare3JHH(FeatureEngineer):
     def feature_cols():
         return ['distance', 'distance_0x', 'distance_xy', 'distance_y1', 'distance_0y', 'distance_x1',
                 'x_nitrogen', 'x_oxygen', 'y_nitrogen', 'y_oxygen',
-                'x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors',
-                'y_H_neighbors', 'y_C_neighbors', 'y_N_neighbors', 'y_O_neighbors',
+                'x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors', 'x_F_neighbors',
+                'y_H_neighbors', 'y_C_neighbors', 'y_N_neighbors', 'y_O_neighbors', 'y_F_neighbors',
                 'x_sp', 'x_sp2', 'x_sp3', 'y_sp', 'y_sp2', 'y_sp3',
                 'dihedral', 'cos_theta', 'cos_2theta']
 
@@ -324,8 +330,8 @@ class Prepare3JHH(FeatureEngineer):
         df = self.get_element_encodings(df, 'atom_index_x', x_element_cols)
         df = self.get_element_encodings(df, 'atom_index_y', y_element_cols)
 
-        x_neighbor_cols = ['x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors']
-        y_neighbor_cols = ['y_H_neighbors', 'y_C_neighbors', 'y_N_neighbors', 'y_O_neighbors']
+        x_neighbor_cols = ['x_H_neighbors', 'x_C_neighbors', 'x_N_neighbors', 'x_O_neighbors', 'x_F_neighbors']
+        y_neighbor_cols = ['y_H_neighbors', 'y_C_neighbors', 'y_N_neighbors', 'y_O_neighbors', 'y_F_neighbors']
         df = self.get_neighbors(df, 'atom_index_x', x_neighbor_cols)
         df = self.get_neighbors(df, 'atom_index_y', y_neighbor_cols)
 
@@ -355,13 +361,13 @@ class Prepare3JH_(Prepare3JHH):
 
     @staticmethod
     def feature_cols():
-        return Prepare3JHH.feature_cols() + ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors',
+        return Prepare3JHH.feature_cols() + ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors', 'F_neighbors',
                                              'sp', 'sp2', 'sp3']
 
     def __call__(self, df):
         df = super().__call__(df)
 
-        neighbor_cols = ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors']
+        neighbor_cols = ['H_neighbors', 'C_neighbors', 'N_neighbors', 'O_neighbors', 'F_neighbors']
         df = self.get_neighbors(df, 'atom_index_1', neighbor_cols)
 
         hybrid_cols = ['sp', 'sp2', 'sp3']
