e79dd612298be90d3e7eb08982ce7ed3f94debd0
Martin Chobanyan <chobanyan.martin@gmail.com>
2019-08-13 22:35:50 -0400

    average can now incoporate more than two submission files

--- average_results.py
+++ average_results.py
@@ -2,16 +2,23 @@
 # -*- coding: utf-8 -*-
 """This script performs a weighted average of different submission results into a final submission csv file"""
 import os
+import numpy as np
 import pandas as pd
 
 
 SUBMISSION_DIR = '/home/mchobanyan/data/kaggle/molecules/submissions'
 output_path = os.path.join(SUBMISSION_DIR, 'submission.csv')
 
-rf_df = pd.read_csv(os.path.join(SUBMISSION_DIR, 'pairwise_submission.csv'))
-gat_df = pd.read_csv(os.path.join(SUBMISSION_DIR, 'gat_submission.csv'))
+index = None
+predictions = None
+weights = {'pairwise': 0.3, 'gat': 0.2, 'edgeconv': 0.5}
+for model_name, weight in weights.items():
+    print(model_name)
+    df = pd.read_csv(os.path.join(SUBMISSION_DIR, f'{model_name}_submission.csv'))
+    if index is None:
+        index = df['id'].values
+        predictions = np.zeros(len(index))
+    predictions += weight * df['scalar_coupling_constant'].values
 
-df = rf_df.merge(gat_df, on=['id'], suffixes=['_rf', '_gat'])
-df['scalar_coupling_constant'] = 0.4 * df['scalar_coupling_constant_rf'] + 0.6 * df['scalar_coupling_constant_gat']
-df = df[['id', 'scalar_coupling_constant']]
+df = pd.DataFrame({'id': index, 'scalar_coupling_constant': predictions})
 df.to_csv(output_path, index=False)
